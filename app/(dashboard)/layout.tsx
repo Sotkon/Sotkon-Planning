5ef290ab502d16826f8c9268004ffdd8ec068992
sotkonhsilva <jhsilva@sotkon.com>
2025-12-13 23:55:20 +0000

    Change background color from gray to neutral

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -16,7 +16,7 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-neutral-800">
       <Navbar />
       <main className="container mx-auto py-6 px-4">
         {children}
