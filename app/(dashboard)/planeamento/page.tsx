04246e776b30b018e581b1deec38b5357e7c0845
sotkonhsilva <jhsilva@sotkon.com>
2025-12-15 14:50:10 +0000

    Update page.tsx

--- app/(dashboard)/planeamento/page.tsx
+++ app/(dashboard)/planeamento/page.tsx
@@ -1,11 +1,7 @@
 // app/(dashboard)/planeamento/page.tsx
-export default function PlaneamentoPage() {
-  return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Planeamento/h1>
       <div className="bg-neutral-800 rounded-lg shadow p-6">
         <p>Planeamento vai aparecer aqui...</p>
       </div>
     </div>
-  )
-}
