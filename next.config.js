41020c555d204f2ff2364e92fbdd367a6bcebb16
Arinto <70295866+arinto00@users.noreply.github.com>
2026-01-10 02:02:15 +0000

    Configure Next.js to bundle Prisma correctly

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Incluir arquivos do Prisma no build
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@prisma/client': 'commonjs @prisma/client'
+      });
+    }
+    return config;
+  },
+  // Copiar schema do Prisma
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  }
 }
 
 module.exports = nextConfig
